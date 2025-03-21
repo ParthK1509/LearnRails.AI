11e3e3db3849354116008a29d35399a528f3660c
Chayan <chayanhjain@gmail.com>
2025-03-21 19:23:33 +0530

    home

---
+++ src/home/home.jsx
@@ -0,0 +1,15 @@
+import "./home.css";
+export default function Home() {
+  return (
+    <div className="root">
+      <div className="logo">
+        <div className="heading">LearnRails.AI</div>
+        <div className="subtitle">AI Powered Learning Path for You</div>
+      </div>
+      <div className="main">
+        <p className="whichTopic">Which Topic are we learning today?</p>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
